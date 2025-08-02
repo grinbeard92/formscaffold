a48d83e5d344320b1dd1bcbc22853838ea9ac461
bgreer <greergit@proton.me>
2025-08-02 10:56:30 -0500

    Prettier and git LF config

---
+++ src/utils/trpc.ts
@@ -0,0 +1,48 @@
+// @SSR SETUP
+// This file is used to set up tRPC for server-side rendering (SSR) in a Next.js application.
+//
+
+import { httpBatchLink } from "@trpc/client";
+import { createTRPCNext } from "@trpc/next";
+import { ssrPrepass } from "@trpc/next/ssrPrepass";
+import superjson from "superjson";
+import type { AppRouter } from "@/api/trpc/[trpc]";
+export const trpc = createTRPCNext<AppRouter>({
+  ssr: true,
+  ssrPrepass,
+  config(opts) {
+    const { ctx } = opts;
+    if (typeof window !== "undefined") {
+      // during client requests
+      return {
+        links: [
+          httpBatchLink({
+            url: "/api/trpc",
+          }),
+        ],
+      };
+    }
+    return {
+      links: [
+        httpBatchLink({
+          // The server needs to know your app's full url
+          url: `${getBaseUrl()}/api/trpc`,
+          /**
+           * Set custom request headers on every request from tRPC
+           * @see https://trpc.io/docs/v10/header
+           */
+          headers() {
+            if (!ctx?.req?.headers) {
+              return {};
+            }
+            // To use SSR properly, you need to forward client headers to the server
+            // This is so you can pass through things like cookies when we're server-side rendering
+            return {
+              cookie: ctx.req.headers.cookie,
+            };
+          },
+        }),
+      ],
+    };
+  },
+});
